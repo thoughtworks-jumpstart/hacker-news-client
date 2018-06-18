272bd39eedb2f768b727fc85035f888e80822c67
davified <davified@gmail.com>
2018-06-18 16:56:18 +0800

    Add some solutions

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
+import { articles } from "../seedData";
 
 class NewsFeed extends Component {
   constructor() {
     super();
     this.state = {
-      articles: []
+      articles: articles
     };
   }
 
   render() {
     return this.state.articles.map((article, i) => {
-      
+      return (
+        <div key={i}>
+          <h3>
+            {" "}
+            <a href={article.url}>{article.title}</a>
+          </h3>
+          <p>{article.author}</p>
+          <img src={article.urlToImage} alt="" />
+          <p>{article.description}</p>
+          <div>Upvotes: {article.votes}</div>
+        </div>
+      );
     });
   }
 }
